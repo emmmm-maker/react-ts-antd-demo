7455b7aa71dc093b38bb2a84e0b23f160cd66dfd
GG-Boy <xing_chaochao@sina.com>
2023-06-30 16:17:27 +0800

    feat(utils/request): 二次封装axios

---
+++ src/utils/request.ts
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+const request = axios.create({
+  baseURL: 'http://vapi.youlai.tech/api/v1/',
+  timeout: 5000,
+})
+
+request.interceptors.request.use(
+  (config) => {
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+request.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+export default request
